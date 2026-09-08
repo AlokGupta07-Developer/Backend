220ca27bf15177ba5ffbfed758303ea196c2b045
Alok07Developer <alokguptamohit@gmail.com>
2026-09-08 13:54:44 +0530

    changes

--- day-8/Frontend/src/App.jsx
+++ day-8/Frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   ]);
 
   function FetchNotes() {
-    axios.get("http://localhost:3000/api/notes").then((res) => {
+    axios.get("https://full-stack-t2ns.onrender.com/api/notes").then((res) => {
       setNotes(res.data.notes);
     });
   }
@@ -37,7 +37,7 @@ const App = () => {
 
   console.log(title.value, description.value);
   
-  axios.post('http://localhost:3000/api/notes',{
+  axios.post('https://full-stack-t2ns.onrender.com/api/notes',{
     title: title.value,
     description: description.value
   })
@@ -49,7 +49,7 @@ const App = () => {
 }
 
 function handleDeleteNote(noteId) {
-  axios.delete('http://localhost:3000/api/notes'+noteId)
+  axios.delete('https://full-stack-t2ns.onrender.com/api/notes'+noteId)
   .then((res)=>{
     console.log(res.data)
    
