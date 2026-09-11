048c478a7bdf965c9f9b1fd3a9ce219a927272c7
Alok07Developer <alokguptamohit@gmail.com>
2026-09-11 11:12:01 +0530

    modify readme.md

--- day-server/server.js
+++ day-server/server.js
@@ -1,3 +1,17 @@
 const app = require("./src/app")
+const mongoose = require("mongoose")
 
-app.listen(3000)
+
+function connectToDb() {
+    mongoose.connect("mongodb+srv://alok-07:Alok18072003@cluster0.essbbdt.mongodb.net/day-server")
+    .then(()=>{
+        console.log("Database connected")
+    })
+}
+
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000")
+
+})
--- day-server/src/app.js
+++ day-server/src/app.js
@@ -1,27 +1,6 @@
 const express = require("express")
 
 const app = express()
-app.use(express.json())
-
-const notes = []
-
-app.post("/notes",(req,res)=>{
-    console.log(req.body)
-    notes.push(req.body)
-    console.log(notes)
-
-    res.send("Notes created...")
-})
-
-app.get("/notes",(req,res)=>{
-    res.send(notes)
-})
-
-app.delete("/notes/:index",(req,res)=>{
-    console.log(req.params.index)
-    delete notes[req.params.index]
-    res.send("Notes deleted")
-})
 
 
 module.exports = app
